faf1872fc9245a1489c87870a60f2ec224238dc4
Koichi Sakyo <sakyo@tmr.co.jp>
2020-07-27 17:48:08 +0900

    えへへ

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteTitle: 'デザイン＆テクノロジー合同会社', // Site title.
-  siteTitleAlt: 'Design & Technology LLC.', // Alternative site title for SEO.
+  siteTitleAlt: 'デザイン＆テクノロジー', // Alternative site title for SEO.
   siteLogo: '/icons/D&T.png', // Logo used for SEO and manifest.
   siteUrl: 'https://www.design-and-tech.com', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
@@ -11,7 +11,7 @@ module.exports = {
   disqusShortname: 'design-and-technology', // Disqus shortname.
   userName: 'Ore Ore',
   userTwitter: '',
-  userLocation: 'Umeyashiki Ota Tokyo, Japan',
+  userLocation: '東京都大田区大森西6-17-17 KOCA A3',
   userDescription: 'デザイン＆テクノロジー',
   copyright: 'Copyright © Design & Technology LLC. 2020. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#ff9900', // Used for setting manifest and progress theme colors.
